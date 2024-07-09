69c0024c2044362251ea9ef8cf86c708259021c5
akhil <akhilreddy723@gmail.com>
2024-07-09 23:38:07 +0530

    deleted files

--- AkhilReddy-java-master/src/main/java/org/firstAPI/Controller/MyFirstController.java
+++ AkhilReddy-java-master/src/main/java/org/firstAPI/Controller/MyFirstController.java
@@ -1,12 +1,13 @@
 package org.firstAPI.Controller;
 
+import org.firstAPI.Services.MyFirstService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyFirstController {
 
-    @getMapping
-    public String myFirstAPI(){
-        return "Hello world from Controller";
-    }
+    @Autowired
+    MyFirstService myFirstService;
+
 }
---
+++ AkhilReddy-java-master/src/main/java/org/firstAPI/Services/MyFirstService.java
@@ -0,0 +1,12 @@
+package org.firstAPI.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyFirstService {
+
+    public String myFirstAPI(){
+        return "Hello world from Controller";
+    }
+
+}
